6c1855cff89e54682a78085946b925f0a1916646
Juan Abadia <juan.abadia@gmail.com>
2025-06-06 09:19:30 +0200

    select returns exact number of points

--- flow_nodes/select.gd
+++ flow_nodes/select.gd
@@ -1,6 +1,6 @@
 extends FlowNodeBase
 
-@export var ratio : float = 0.1
+@export var ratio : float = 0.2
 
 static func getMeta() -> Dictionary :
 	return {
@@ -10,9 +10,17 @@ static func getMeta() -> Dictionary :
 	}
 
 func execute( ):
-	var input_data = get_input(0)
-	var output = []
-	for data in input_data:
-		if rng.randf() < ratio:
-			output.append( data )
-	set_output( 0, output )
+	var in_data = get_input(0)
+	var out_data = []
+	var in_size = in_data.size()
+	var out_size = round(in_size * ratio)
+	# print( "From %d, took %1.2f%% -> %d" % [ in_size, ratio, out_size ])
+	
+	var pool := range(in_size)
+	pool.shuffle()
+	var subset := pool.slice(0, out_size)
+	subset.sort()
+	
+	for idx in subset:
+		out_data.append( in_data[idx] )
+	set_output( 0, out_data )
