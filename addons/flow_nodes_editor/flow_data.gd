6ea8763a32bece3b9c472d1a4fe511c3884ee01c
Juan Abadia <juan.abadia@gmail.com>
2025-06-07 15:02:03 +0200

    Filter working

--- addons/flow_nodes_editor/flow_data.gd
+++ addons/flow_nodes_editor/flow_data.gd
@@ -25,24 +25,26 @@ class Data:
 		
 	func size() -> int:
 		var key0 = streams.keys()[0]
-		return streams[ key0 ].size()
+		return streams[ key0 ].container.size()
+	
+	func registerStream( name : String, data_type : DataType, container ):
+		streams[ name ] = { 
+			"container" : container,
+			"name" : name,
+			"data_type" : data_type
+		}	
 	
 	func addStream( data_type : DataType, name : String ):
-		
 		if streams.has( name ):
 			push_error("Data already has stream named %s" % name )
 			return null
 		var new_container = newContainerOfType(data_type)
-		streams[ name ] = { 
-			"container" : new_container,
-			"name" : name,
-			"data_type" : data_type
-		}
+		registerStream( name, data_type, new_container )
 		return new_container
 		
 	func cloneStream( name : String ):
 		if not streams.has( name ):
-			push_error("Data does not have a stream named %s" % name )
+			push_error("cloneStream: Data does not have a stream named %s" % name )
 			return null
 		var prev_stream = streams[ name ]
 		var new_container
@@ -55,14 +57,49 @@ class Data:
 				new_container = PackedVector3Array( prev_stream.container )		
 		prev_stream.container = new_container
 		return new_container
+		
+	func filteredStream( old_stream : Dictionary, indices : PackedInt32Array ):
+		var new_size : int = indices.size()
+		match old_stream.data_type:
+			DataType.Bool:
+				var old_container : PackedByteArray = old_stream.container
+				var new_container = PackedByteArray( )
+				new_container.resize( new_size )
+				for idx in range( new_size ):
+					new_container[idx] = old_container[ indices[idx] ]
+				return new_container
+			DataType.Float:
+				var old_container : PackedFloat32Array = old_stream.container
+				var new_container = PackedFloat32Array( )
+				new_container.resize( new_size )
+				for idx in range( new_size ):
+					new_container[idx] = old_container[ indices[idx] ]
+				return new_container
+			DataType.Vector:
+				var old_container : PackedVector3Array = old_stream.container
+				var new_container = PackedVector3Array(  )		
+				new_container.resize( new_size )
+				for idx in range( new_size ):
+					new_container[idx] = old_container[ indices[idx] ]
+				return new_container
+		return null
 
 	func duplicate():
+		# This is not a deep close, the packed*arrays are shared,
+		# use cloneStream to create an independent cpy
 		var s = Data.new()
 		s.streams = streams.duplicate()
 		return s
+		
+	func filter( indices : PackedInt32Array ):
+		var new_data = Data.new()
+		for old_stream in streams.values():
+			var new_container = filteredStream( old_stream, indices )
+			new_data.registerStream( old_stream.name, old_stream.data_type, new_container )
+		return new_data
 
 	func dump( title : String ):
-		print( "== %s" % title)
+		print( "== %s (%d streams) ==" % [title, streams.size()] )
 		for stream in streams.values():
 			print( "%s (%s) %d elems" % [ stream.name, stream.data_type, stream.container.size() ] )
 			for data in stream.container:
--- addons/flow_nodes_editor/nodes/select.gd
+++ addons/flow_nodes_editor/nodes/select.gd
@@ -11,19 +11,18 @@ func getMeta() -> Dictionary :
 	}
 
 func execute( ):
-	var in_data = get_input(0)
+	var in_data : FlowData.Data = get_input(0)
+	in_data.dump( "Select.Input")
 	var in_size = in_data.size()
 	var out_size = round(in_size * settings.ratio)
-	## print( "From %d, took %1.2f%% -> %d" % [ in_size, ratio, out_size ])
+	print( "Select: From %d, took %1.2f%% -> %d" % [ in_size, settings.ratio, out_size ])
 	
 	var pool := range(in_size)
 	shuffleArray( pool )
 	var subset := pool.slice(0, out_size)
 	subset.sort()
+	var indices := PackedInt32Array( subset )
+	print( "select.indices", indices )
 	
-	#return in_data.filter( subset )
-	
-	#for idx in subset:
-		#out_data.append( in_data[idx] )
-	#set_output( 0, out_data )
-	pass
+	var out_data = in_data.filter( indices )
+	set_output( 0, out_data )
