50f14534c01bcaa753b56fbc859382ee10f127dd
Juan Abadia <juan.abadia@gmail.com>
2025-06-11 13:45:48 +0200

    Add Attribute node

---
+++ addons/flow_nodes_editor/nodes/add_attribute.gd
@@ -0,0 +1,46 @@
+@tool
+extends FlowNodeBase
+
+func getMeta() -> Dictionary :
+	return {
+		"title" : "Add Attribute",
+		"settings" : AddAttributeNodeSettings,
+		"ins" : [{"label": "In A" }], 
+		"outs" : [{ "label" : "Out" }],
+	}
+	
+func getTitle() -> String:
+	return "%s - %s" % [ settings.name, FlowData.DataType.keys()[settings.data_type] ]
+
+func execute( _ctx : FlowData.EvaluationContext ):
+	var in_data : FlowData.Data = get_input(0)
+	var out_data : FlowData.Data = in_data.duplicate()
+	
+	match settings.data_type:
+		
+		FlowData.DataType.Float:
+			var new_val : float = settings.cte_float
+			var sout : PackedFloat32Array = out_data.addStream( settings.name, settings.data_type )
+			for i in sout.size():
+				sout[i] = new_val
+				
+		FlowData.DataType.Vector:
+			var new_val : Vector3 = settings.cte_vector
+			var sout : PackedVector3Array = out_data.addStream( settings.name, settings.data_type )
+			for i in sout.size():
+				sout[i] = new_val
+				
+		FlowData.DataType.String:
+			var new_val : String = settings.cte_string
+			var sout : Array[String] = out_data.addStream( settings.name, settings.data_type )
+			for i in sout.size():
+				sout[i] = new_val
+				
+		FlowData.DataType.Resource:
+			var new_val : Resource = settings.cte_resource
+			print( "Adding value %s" % new_val )
+			var sout : Array[Resource] = out_data.addStream( settings.name, settings.data_type )
+			for i in sout.size():
+				sout[i] = new_val
+
+	set_output( 0, out_data )
---
+++ addons/flow_nodes_editor/nodes/add_attribute_settings.gd
@@ -0,0 +1,21 @@
+@tool
+class_name AddAttributeNodeSettings
+extends NodeSettings
+
+@export_group("Add Attribute")
+
+@export var name : String = "new_attr"
+@export var data_type : FlowData.DataType = FlowData.DataType.Float:
+	set(new_value):
+		data_type = new_value
+		notify_property_list_changed()
+		
+@export var cte_bool: bool = false
+@export var cte_float : float = 0.0
+@export var cte_vector : Vector3 = Vector3.ZERO
+@export var cte_resource : Resource
+@export var cte_string : String = ""
+
+func _init():
+	super._init()
+	resource_name = "Add Attribute"
