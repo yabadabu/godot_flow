a0a70537cafff12ce56083f624e1e292c1c204b5
Juan Abadia <juan.abadia@gmail.com>
2025-07-14 21:21:18 +0200

    kdtree in bulk in c++

--- plugin/gd_kdtree.cpp
+++ plugin/gd_kdtree.cpp
@@ -8,6 +8,7 @@ using namespace godot;
 void GDKdTree::_bind_methods() {
   ClassDB::bind_method(D_METHOD("set_points"), &GDKdTree::set_points);
   ClassDB::bind_method(D_METHOD("find_nearest_idx"), &GDKdTree::find_nearest_idx);
+  ClassDB::bind_method(D_METHOD("find_nearest_indices"), &GDKdTree::find_nearest_indices);
 }
 
 GDKdTree::GDKdTree( ) {
@@ -28,10 +29,27 @@ int GDKdTree::find_nearest_idx( const Vector3& pos ) const {
 }
 
 PackedInt32Array GDKdTree::find_nearest_indices( const PackedVector3Array& in_pos ) const {
+  size_t num_elems = in_pos.size();
+
+  // Hold nearest indices
   PackedInt32Array idxs;
+  idxs.resize( num_elems );
+
+  // Setup 
+  nanoflann::KNNResultSet<float> results(1);
+  size_t nearest_idx;
+  float out_distance;
+  results.init(&nearest_idx, &out_distance);
+
+  // This could be executed in parallel
+  const Vector3* pos_addr = in_pos.ptr();
+  for( size_t i=0; i<num_elems; ++i, ++pos_addr ) {
+    tree->findNeighbors(results, &pos_addr->x, nanoflann::SearchParams(3));
+    idxs[ i ] = nearest_idx;
+  }
+
   return idxs;
 } 
-
 
 void GDKdTree::set_points( const PackedVector3Array& in_pos ) {
   all.points = in_pos;
