6799f88fe289e2bba33f1915e30523dd540ff9eb
Juan Abadia <juan.abadia@gmail.com>
2025-07-18 19:08:12 +0200

    reduce reduced :)

--- demo/addons/flow_nodes_editor/nodes/reduce.gd
+++ demo/addons/flow_nodes_editor/nodes/reduce.gd
@@ -9,6 +9,28 @@ func _init():
 		"outs" : [{ "label" : "Out" }],
 	}
 	
+func addSingleValue( out_data : FlowData.Data, stream_name : String, value ):
+	var container
+	if typeof( value ) == TYPE_FLOAT:
+		container = PackedFloat32Array()
+	elif typeof( value ) == TYPE_VECTOR3:
+		container = PackedVector3Array()
+	elif typeof( value ) == TYPE_INT:
+		container = PackedInt32Array()
+	container.append( value )
+	out_data.registerStream( stream_name, container )
+	
+func addReducedValues( out_data : FlowData.Data, vmin, vmax, vavg ):
+	var out_prefix = settings.out_prefix
+	if !out_prefix:
+		out_prefix = settings.in_name.replace( ".", "_")
+	var cmin_name = out_prefix + "_min"
+	var cmax_name = out_prefix + "_max"
+	var cavg_name = out_prefix + "_avg"
+	addSingleValue( out_data, cmin_name, vmin )
+	addSingleValue( out_data, cmax_name, vmax )
+	addSingleValue( out_data, cavg_name, vavg )
+	
 func execute( ctx : FlowData.EvaluationContext ):
 
 	if !settings.in_name:
@@ -26,14 +48,6 @@ func execute( ctx : FlowData.EvaluationContext ):
 
 	var num_elems := in_data.size()
 
-	var out_prefix = settings.out_prefix
-	if !out_prefix:
-		out_prefix = settings.in_name.replace( ".", "_")
-
-	var cmin_name = out_prefix + "_min"
-	var cmax_name = out_prefix + "_max"
-	var cavg_name = out_prefix + "_avg"
-	
 	var out_data := FlowData.Data.new()
 	if num_elems > 0:
 		var gmin
@@ -49,13 +63,7 @@ func execute( ctx : FlowData.EvaluationContext ):
 				vmax = maxf( vmax, inA[idx] )
 				vacc += inA[idx]
 			vacc /= float(num_elems)
-
-			gmin = PackedFloat32Array()
-			gmin.append( vmin )
-			gmax = PackedFloat32Array()
-			gmax.append( vmax )
-			gavg = PackedFloat32Array()
-			gavg.append( vacc )
+			addReducedValues( out_data, vmin, vmax, vacc )
 
 		elif sA.data_type == FlowData.DataType.Int:
 			var inA : PackedInt32Array = sA.container
@@ -67,13 +75,7 @@ func execute( ctx : FlowData.EvaluationContext ):
 				vmax = max( vmax, inA[idx] )
 				vacc += inA[idx]
 			vacc /= float(num_elems)
-
-			gmin = PackedInt32Array()
-			gmin.append( vmin )
-			gmax = PackedInt32Array()
-			gmax.append( vmax )
-			gavg = PackedFloat32Array()
-			gavg.append( vacc )
+			addReducedValues( out_data, vmin, vmax, vacc )
 
 		elif sA.data_type == FlowData.DataType.Vector:
 			var inA : PackedVector3Array = sA.container
@@ -85,16 +87,6 @@ func execute( ctx : FlowData.EvaluationContext ):
 				vmax = vmax.max( inA[idx] )
 				vacc += inA[idx]
 			vacc /= float(num_elems)
-
-			gmin = PackedVector3Array()
-			gmin.append( vmin )
-			gmax = PackedVector3Array()
-			gmax.append( vmax )
-			gavg = PackedVector3Array()
-			gavg.append( vacc )
-
-		out_data.registerStream( cmin_name, gmin )
-		out_data.registerStream( cmax_name, gmax )
-		out_data.registerStream( cavg_name, gavg )
+			addReducedValues( out_data, vmin, vmax, vacc )
 
 	set_output( 0, out_data )
