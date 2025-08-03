6b17b364d5d2bac2146a91c080793626ca92858f
Juan Abadia <juan.abadia@gmail.com>
2025-08-04 00:23:31 +0200

    grid add transtion/rotation

--- demo/addons/flow_nodes_editor/nodes/grid.gd
+++ demo/addons/flow_nodes_editor/nodes/grid.gd
@@ -7,7 +7,7 @@ func _init():
 		"settings" : GridNodeSettings,
 		"ins" : [],
 		"outs" : [{ "label" : "Out" }],
-		"tooltip" :"Generates a set of points in a grid spatial distribution,\nwhere the separation is step",
+		"tooltip" : "Generates a set of points in a grid spatial distribution,\nwhere the separation is step",
 	}
 
 func execute( ctx : FlowData.EvaluationContext ):
@@ -18,14 +18,19 @@ func execute( ctx : FlowData.EvaluationContext ):
 	var nsamples : int = nx * ny * nz
 	output.addCommonStreams( nsamples )
 	var spos := output.getVector3Container( FlowData.AttrPosition )
+	var srot := output.getVector3Container( FlowData.AttrRotation )
 	assert( spos != null )
 	#print( "Spos.size %d of type %s" % [ spos.size(), type_string(typeof(spos)) ])
 	var idx := 0
+	var origin : Vector3 = getSettingValue( ctx, "origin" )
+	var rotation : Vector3 = getSettingValue( ctx, "rotation" )
 	var step : Vector3 = getSettingValue( ctx, "step" )
+	var transform = Transform3D( FlowData.eulerToBasis(rotation), origin )
 	for iz in range( 0, nz ):
 		for iy in range( 0, ny ):
 			for ix in range( 0, nx ):
 				var p := Vector3( ix, iy, iz ) * step
-				spos[idx] = p
+				spos[idx] = transform * p
+				srot[idx] = rotation
 				idx += 1
 	set_output( 0, output )
--- demo/addons/flow_nodes_editor/nodes/grid_settings.gd
+++ demo/addons/flow_nodes_editor/nodes/grid_settings.gd
@@ -8,6 +8,8 @@ extends NodeSettings
 @export_range( 0, 50 ) var y : int = 1
 @export_range( 0, 50 ) var z : int = 3
 @export var step : Vector3 = Vector3( 1.0, 1.0, 1.0 )
+@export var origin : Vector3 = Vector3.ZERO
+@export var rotation : Vector3 = Vector3.ZERO
 
 func _init():
 	super._init()
