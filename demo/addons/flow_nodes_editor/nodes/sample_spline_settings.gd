ae0421c6bd817fe8aba00172909e3f627b4a5d39
Juan Abadia <juan.abadia@gmail.com>
2025-08-17 17:42:36 +0200

    node sample_spline hides param "sample_segments_centers" when 'fill interior is set'

--- demo/addons/flow_nodes_editor/nodes/sample_spline_settings.gd
+++ demo/addons/flow_nodes_editor/nodes/sample_spline_settings.gd
@@ -5,7 +5,11 @@ extends NodeSettings
 @export_group("Sample Spline")
 
 @export var uniform_interval : float = 0.2
-@export var fill_curve : bool = false
+@export var fill_curve : bool = false:
+	set( new_value ):
+		fill_curve = new_value
+		notify_property_list_changed()
+		
 @export var sample_segments_centers : bool = false
 @export var required_meta_bool : StringName
 @export var distance_attribute : String = "distance"
@@ -13,3 +17,8 @@ extends NodeSettings
 func _init():
 	super._init()
 	resource_name = "Sample Spline Settings"
+
+func exposeParam( name : String ) -> bool:
+	if name == "sample_segments_centers":
+		return not fill_curve
+	return true
