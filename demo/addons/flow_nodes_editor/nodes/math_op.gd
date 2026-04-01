35d3dbba017cdb172cdefa585dbd4c720356aaf6
Juan Abadia <jabadia@mobilemediacontent.com>
2026-04-01 21:31:24 +0200

    Fix prev merge Upload the precompiled windows.editor tweak the demo_relax

--- demo/addons/flow_nodes_editor/nodes/math_op.gd
+++ demo/addons/flow_nodes_editor/nodes/math_op.gd
@@ -159,9 +159,6 @@ func execute( _ctx : FlowData.EvaluationContext ):
 				MathOpNodeSettings.eOperation.Add:
 					for i in num_elems:
 						outC[i] = inA[i] + inB[i]
-				MathOpNodeSettings.eOperation.Set:
-					for i in num_elems:
-						outC[i] = inB[i]
 				MathOpNodeSettings.eOperation.Substract:
 					for i in num_elems:
 						outC[i] = inA[i] - inB[i]
