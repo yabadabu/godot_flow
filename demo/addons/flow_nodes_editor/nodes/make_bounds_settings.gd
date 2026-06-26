dce743cec932e067bcb9ab3706f2e987e8e16263
Juan Abadia <jabadia@mobilemediacontent.com>
2026-06-26 17:04:22 +0200

    Discovered the PROPERTY_HINT_LINK

--- demo/addons/flow_nodes_editor/nodes/make_bounds_settings.gd
+++ demo/addons/flow_nodes_editor/nodes/make_bounds_settings.gd
@@ -3,7 +3,8 @@ class_name MakeBoundsNodeSettings
 extends NodeSettings
 
 @export_group("Make Bounds")
-@export var size: Vector3 = Vector3(48.0, 1.0, 48.0)
+@export_custom(PROPERTY_HINT_LINK, "")
+var size: Vector3 = Vector3(48.0, 1.0, 48.0)
 @export var center: Vector3 = Vector3.ZERO
 
 func _init():
