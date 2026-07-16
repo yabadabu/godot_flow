a609b95fe6ee24d8df7073cc312bf8aafc94bc20
Juan Abadia <jabadia@mobilemediacontent.com>
2026-07-16 18:59:03 +0200

    Recover attribute random

--- demo/addons/flow_nodes_editor/nodes/attribute_random.gd
+++ demo/addons/flow_nodes_editor/nodes/attribute_random.gd
@@ -1,12 +1,17 @@
 @tool
 extends FlowNodeBase
 
-const AttributeRandomNodeSettings = preload("res://addons/flow_nodes_editor/nodes/attribute_random_settings.gd")
+@export var attribute_name: String = "random_attr"
+
+enum eType { Float, Int }
+@export var data_type: eType = eType.Float
+
+@export var min_value: float = 0.0
+@export var max_value: float = 1.0
 
 func _init():
 	meta_node = {
 		"title" : "Attribute Random",
-		"settings" : AttributeRandomNodeSettings,
 		"category" : "Metadata",
 		"ins" : [{ "label": "In" }], 
 		"outs" : [{ "label" : "Out" }],
@@ -22,19 +27,19 @@ func execute( ctx : FlowData.EvaluationContext ):
 	var out_data : FlowData.Data = in_data.duplicate()
 	var size := in_data.size()
 	
-	if settings.data_type == AttributeRandomNodeSettings.eType.Int:
+	if data_type == eType.Int:
 		var container := PackedInt32Array()
 		container.resize(size)
-		var imin := int(settings.min_value)
-		var imax := int(settings.max_value)
+		var imin := int(min_value)
+		var imax := int(max_value)
 		for i in range(size):
 			container[i] = rng.randi_range(imin, imax)
-		out_data.registerStream(settings.attribute_name, container, FlowData.DataType.Int)
+		out_data.registerStream(attribute_name, container, FlowData.DataType.Int)
 	else:
 		var container := PackedFloat32Array()
 		container.resize(size)
 		for i in range(size):
-			container[i] = rng.randf_range(settings.min_value, settings.max_value)
-		out_data.registerStream(settings.attribute_name, container, FlowData.DataType.Float)
+			container[i] = rng.randf_range(min_value, max_value)
+		out_data.registerStream(attribute_name, container, FlowData.DataType.Float)
 		
 	set_output(0, out_data)
